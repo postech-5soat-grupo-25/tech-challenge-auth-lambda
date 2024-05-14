ce92822804efd73d00d1ad78fc858ddf1651d4c3
Matheus Bachiste Lopes <matheusblopes10@gmail.com>
2024-05-13 21:13:57 -0300

    feat: update pool id

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const AWS = require('aws-sdk');
 
 const app = express();
 
-const USER_POOL_ID = 'us-east-1_9KnwgXBoZ';
+const USER_POOL_ID = 'us-east-1_cHfnf8rNY';
 
 const isCpfValid = (cpf) =>
 {
